53df11762fb9da9d96aac50e071ce8f5a4d0d349
Siva Sankar Chandu <57555760+SivaSankarChandu@users.noreply.github.com>
2023-01-17 18:41:53 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,31 +1,33 @@
 import requests
 import json
 
-# Set up the API endpoint and access token
-api_url = "https://api.box.com/2.0/files/"
-access_token = "ASq8MSgjUuMrFSuoZ6f8OwxtMVnAwrSl"
-# The ID of the file to be copied
-file_id = "1113023725040"
+python script run through the postman to copy files in box cloud
 
-# The ID of the parent folder where the copies will be created
-parent_folder_id = "190182795566"
+import requests
+import json
 
-# Create a loop to copy the file 1000 times
-for i in range(100):
-    # Set up the data for the API request
-    data = {
-        "parent": {"id": parent_folder_id},
-        "name": f"File1 {i+1}.xlsx"
-    }
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "As-User": "18199389449",
-        "Content-Type": "application/json"
-    }
-    # Make the API request to copy the file
-    response = requests.post(f"{api_url}{file_id}/copy", data=json.dumps(data), headers=headers)
-    if response.status_code != 201:
-        print(f"Error copying file: {response.json()}")
-        break
+# Set the request parameters
+url = 'https://api.box.com/2.0/files/content'
 
-print("All files copied successfully!")
+# Specify the file to be copied
+file_id = '1113023725040'
+
+# Set proper headers
+headers = {'Authorization':'Bearer ASq8MSgjUuMrFSuoZ6f8OwxtMVnAwrSl',
+           'Content-Type':'application/json'}
+
+# Set the parameters
+data={ "name": "filename_copy.txt",
+       "parent": { "id": "190182795566" } }
+
+# Do the HTTP post request
+response = requests.post(url, data=json.dumps(data), headers=headers)
+
+# Check for HTTP codes other than 200
+if response.status_code != 200:
+    print('Status:', response.status_code, 'Problem with the request. Exiting.')
+    exit()
+
+# Decode the JSON response into a dictionary and use the data
+data = response.json()
+print(data)
